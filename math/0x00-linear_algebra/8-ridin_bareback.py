89db5135c90df7c1f978ee6f3c145d4978422e02
zahraaassaad <zahraa.asaad8@gmail.com>
2020-12-04 08:59:06 +0000

    None

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -7,6 +7,8 @@ Module for mat_mul.
 
 def mat_mul(mat1, mat2):
     """Multiply 2 arrays."""
+    if len(mat1[0]) != len(mat2):
+        return None
     new_mat = [[sum(a * b for a, b in zip(mat1_row, mat2_col))
                 for mat2_col in zip(*mat2)]
                for mat1_row in mat1]
