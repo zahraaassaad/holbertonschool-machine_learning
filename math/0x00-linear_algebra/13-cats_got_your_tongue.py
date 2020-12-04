d3fef982d4eb4e980f73331d722495c0976f304a
zahraaassaad <zahraa.asaad8@gmail.com>
2020-12-04 10:50:00 +0000

    13

---
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Module for np_cat.
+"""
+import numpy as np
+
+
+def np_cat(mat1, mat2, axis=0):
+    """Concatenate 2 np arrays."""
+    return(np.concatenate((mat1, mat2), axis = axis))
