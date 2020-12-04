931457cc8e636d3b817017d02c53323a352bb321
zahraaassaad <zahraa.asaad8@gmail.com>
2020-12-04 10:51:48 +0000

    PEP

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -8,4 +8,4 @@ import numpy as np
 
 def np_cat(mat1, mat2, axis=0):
     """Concatenate 2 np arrays."""
-    return(np.concatenate((mat1, mat2), axis = axis))
+    return(np.concatenate((mat1, mat2), axis=axis))
