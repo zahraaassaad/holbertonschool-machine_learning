1dcdb617bfd6af4fc890baaebc6a766f3f0fe39d
zahraaassaad <zahraa.asaad8@gmail.com>
2020-12-04 10:42:36 +0000

    normal ops

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import numpy as np
+
 """
 Module for np_elementwise.
 """
@@ -7,8 +7,4 @@ Module for np_elementwise.
 
 def np_elementwise(mat1, mat2):
     """Operate on np array."""
-    add = np.add(mat1, mat2)
-    sub = np.subtract(mat1, mat2)
-    mul = np.multiply(mat1, mat2)
-    div = np.divide(mat1, mat2)
-    return(add, sub, mul, div)
+    return(mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
