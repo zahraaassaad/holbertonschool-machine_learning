118b46010005bcfe48d347d587ee78b63f438edd
zahraaassaad <zahraa.asaad8@gmail.com>
2020-12-04 08:15:59 +0000

    pep

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -14,7 +14,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
         for row in mat2:
             new_mat.append(row[:])
         return new_mat
-    if axis ==1 and len(mat1) == len(mat2):
+    if axis == 1 and len(mat1) == len(mat2):
         for row in range(len(mat1)):
             new_mat.append(mat1[row] + mat2[row])
         return new_mat
