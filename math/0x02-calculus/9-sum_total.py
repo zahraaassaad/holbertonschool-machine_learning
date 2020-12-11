3a7895dfb0dbd2367cf24249dd09d443da1da927
zahraaassaad <zahraa.asaad8@gmail.com>
2020-12-11 08:59:17 +0000

    9

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,7 +4,7 @@
 
 def summation_i_squared(n):
         """Module for summation of n squared."""
-            if type(n) != int or n < 1:
-                        return None
-            else:
-                        return int((n * (n + 1) * (2 * n + 1)) / 6)
+        if type(n) != int or n < 1:
+                return None
+        else:
+                return int((n * (n + 1) * (2 * n + 1)) / 6)
