eb501aeddf9b74c205cb367ea641f49c588db542
zahraaassaad <69789020+zahraaassaad@users.noreply.github.com>
2020-12-16 17:03:05 +0200

    remove all functions

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -26,24 +26,3 @@ class Normal:
                 for i in range(0, len(data)):
                     s = s + ((data[i] - self.mean))**2
                 self.stddev = (s/len(data))**(1/2)
-
-    def z_score(self, x):
-        """Calculates the z-score."""
-        return ((x - self.mean) / self.stddev)
-
-    def x_value(self, z):
-        """Calculates the x-score."""
-        return self.stddev * z + self.mean
-    
-    def pdf(self, x):
-        """Calculates the value of the PDF."""
-        return (2.7182818285**((-1/2) * (((
-            x - self.mean) / self.stddev)**2))) * (
-            1 / (self.stddev * (2 * self.PI)**(1/2)))
-
-    def cdf(self, x):
-        """Calculates the value of the CDF."""
-        X = (x - self.mean) / (self.stddev * (2**(1/2)))
-        erf = (2 / (3.1415926536**(1/2))) * (X - (X**3)/3 + (
-            X**5)/10 - (X**7)/42 + (X**9)/216)
-        return (1 / 2) * (1 + erf)
