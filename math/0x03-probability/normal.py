0e304821d2d9da2ba26ae615f70d218578a0ce7e
zahraaassaad <zahraa.asaad8@gmail.com>
2020-12-16 14:57:24 +0000

    redone 6

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -8,23 +8,21 @@ Module defining normal distribution
 class Normal:
     """ Normal class. It defines normal distribution.
     """
-
-    PI = 3.1415926536
-
     def __init__(self, data=None, mean=0., stddev=1.):
-        """Initializes the data."""
         if data is None:
             if stddev <= 0:
                 raise ValueError("stddev must be a positive value")
-            self.mean = float(mean)
-            self.stddev = float(stddev)
+            else:
+                self.mean = float(mean)
+                self.stddev = float(stddev)
         else:
-            if not isinstance(data, list):
+            if type(data) != list:
                 raise TypeError("data must be a list")
-            if len(data) < 2:
+            elif len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.mean = sum(data) / len(data)
-            s = 0
-            for i in range(0, len(data)):
-                s += ((data[i] - self.mean))**2
-            self.stddev = (s/len(data))**(1/2) 
+            else:
+                self.mean = (sum(data) / len(data))
+                s = 0
+                for i in range(0, len(data)):
+                    s = s + ((data[i] - self.mean))**2
+                self.stddev = (s/len(data))**(1/2)
