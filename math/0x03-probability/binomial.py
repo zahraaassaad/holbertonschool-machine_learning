2913c12abd2e8ee9c6b218c36e57e3aaf25894e0
zahraaassaad <zahraa.asaad8@gmail.com>
2020-12-16 15:44:53 +0000

    12

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -9,6 +9,7 @@ class Binomial:
     """Binomial class. It defines binomial distribution.
     """
     def __init__(self, data=None, n=1, p=0.5):
+        """Initializes the data."""
         if data is None:
             if n <= 0:
                 raise ValueError("n must be a positive value")
@@ -31,3 +32,34 @@ class Binomial:
                 P = 1 - (var_x / ux)
                 self.n = int(round(ux / P))
                 self.p = float(ux / self.n)
+
+    def pmf(self, k):
+        """Calculates the value of the PMF."""
+        if type(k) != int:
+            k = int(k)
+        if k < 0:
+            return 0
+        factorial_k = 1
+        factorial_n = 1
+        n = self.n
+        factorial_n_k = 1
+        if k != 0:
+            for i in range(1, k + 1):
+                factorial_k = factorial_k * i
+        for j in range(1, n + 1):
+            factorial_n = factorial_n * j
+        for l in range(1, n - k + 1):
+            factorial_n_k = factorial_n_k * l
+        return (factorial_n / (factorial_k * factorial_n_k)) * (
+            self.p**k) * ((1 - self.p)**(self.n - k))
+
+    def cdf(self, k):
+        """Calculates the value of the CDF."""
+        if type(k) != int:
+            k = int(k)
+        if k < 0:
+            return 0
+        CDF = 0
+        for i in range(0, k+1):
+            CDF = CDF + self.pmf(i)
+        return CDF
