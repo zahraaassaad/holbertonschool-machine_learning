39acac96660f1c95fa4af839c8c4e2b63a0fb13a
zahraaassaad <zahraa.asaad8@gmail.com>
2021-01-11 14:12:43 +0000

    up to 17

---
+++ supervised_learning/0x01-classification/17-deep_neural_network.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+16-deep_neural_network.py
+Module defining a deep neural network
+"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """DeepNeuralNetwork class. Defines a deep neural network.
+    """
+    def __init__(self, nx, layers):
+        """Initializes the data."""
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) != list or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integer")
+        for i in layers:
+            if type(i) != int or i <= 0:
+                raise ValueError("layers must be a list of positive integer")
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        layer = 1
+        layer_size = nx
+        for i in layers:
+            if type(i) != int or i <= 0:
+                raise ValueError("layers must be a list of positive integer")
+            w = "W" + str(layer)
+            b = "b" + str(layer)
+            self.__weights[w] = np.random.randn(
+                i, layer_size) * np.sqrt(2/layer_size)
+            self.__weights[b] = np.zeros((i, 1))
+            layer += 1
+            layer_size = i
+
+    @property
+    def L(self):
+        """Getter for L."""
+        return self.__L
+
+    @property
+    def cache(self):
+        """Getter for cache."""
+        return self.__cache
+
+    @property
+    def weights(self):
+        """Getter for weights."""
+        return self.__weights
