0e291330c0e11241f4c32fa51ca81412d66d2f12
zahraaassaad <69789020+zahraaassaad@users.noreply.github.com>
2021-01-12 19:51:53 +0200

    Update 1-create_layer.py

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 def create_layer(prev, n, activation):
     """Returns: the tensor output of the layer."""
     weight = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    layer = tf.layers.dense(units=n, activation=activation,
+    layer = tf.layers.Dense(units=n, activation=activation,
                             kernel_initializer=weight, name="layer")
     y = layer(prev)
     return (y)
