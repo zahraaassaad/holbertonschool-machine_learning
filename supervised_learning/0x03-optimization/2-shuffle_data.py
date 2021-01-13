43a28623fddfdb15ea0297c7225e2421ebb315b8
zahraaassaad <69789020+zahraaassaad@users.noreply.github.com>
2021-01-13 20:14:19 +0200

    Update 2-shuffle_data.py

--- supervised_learning/0x03-optimization/2-shuffle_data.py
+++ supervised_learning/0x03-optimization/2-shuffle_data.py
@@ -8,5 +8,5 @@ import numpy as np
 
 def shuffle_data(X, Y):
     """Returns: the shuffled X and Y matrices."""
-    return np.random.permutation(X),
-    np.random.permutation(Y)
+    perm = np.random.permutation
+    return perm(X), perm(Y)
