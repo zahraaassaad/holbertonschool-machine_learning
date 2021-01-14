462e47706d4b6196005b0c9a627224ac89067851
zahraaassaad <69789020+zahraaassaad@users.noreply.github.com>
2021-01-14 18:28:10 +0200

    Update 3-mini_batch.py

--- supervised_learning/0x03-optimization/3-mini_batch.py
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -26,8 +26,10 @@ def train_mini_batch(X_train, Y_train,
             iterations += 1
             over_size = True
         for epoch in range(epochs + 1):
-            l_train, a_train = sess.run([loss, accuracy],{x: X_train, y: Y_train})
-            l_valid, a_valid = sess.run([loss, accuracy],{x: X_valid, y: Y_valid})
+            l_train, a_train = sess.run([loss, accuracy], {
+                x: X_train, y: Y_train})
+            l_valid, a_valid = sess.run([loss, accuracy], {
+                x: X_valid, y: Y_valid})
             print("After {} epochs:".format(epoch))
             print("\tTraining Cost: {}".format(l_train))
             print("\tTraining Accuracy: {}".format(a_train))
