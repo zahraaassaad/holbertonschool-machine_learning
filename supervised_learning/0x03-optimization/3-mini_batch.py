fd2072c7efbd4df20a66bff8de2af6473d7cfe12
zahraaassaad <69789020+zahraaassaad@users.noreply.github.com>
2021-01-14 18:39:17 +0200

    Update 3-mini_batch.py

--- supervised_learning/0x03-optimization/3-mini_batch.py
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -1,52 +1,47 @@
 #!/usr/bin/env python3
-
-"""trains a loaded neural network model
-using mini-batch gradient descent."""
-
+"""perform a mini-batch train"""
 import tensorflow as tf
-
 shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 
-def train_mini_batch(X_train, Y_train,
-                     X_valid, Y_valid, batch_size=32, epochs=5,
-                     load_path="/tmp/model.ckpt", save_path="/tmp/model.ckpt"):
-    """Returns: the path where the model was saved."""
+def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
+                     epochs=5, load_path="/tmp/model.ckpt",
+                     save_path="/tmp/model.ckpt"):
+    """trains performing minibatch gradient descent"""
     with tf.Session() as sess:
-        saver = tf.train.import_meta_graph(load_path + '.meta')
-        saver.restore(sess, load_path)
-        x = tf.get_collection("x")[0]
-        y = tf.get_collection("y")[0]
-        accuracy = tf.get_collection("accuracy")[0]
-        loss = tf.get_collection("loss")[0]
-        train_op = tf.get_collection("train_op")[0]
-        iterations = X_train.shape[0] // batch_size
-        over_size = False
-        if iterations % batch_size != 0:
-            iterations += 1
-            over_size = True
-        for epoch in range(epochs + 1):
-            l_train, a_train = sess.run([loss, accuracy], {
-                x: X_train, y: Y_train})
-            l_valid, a_valid = sess.run([loss, accuracy], {
-                x: X_valid, y: Y_valid})
-            print("After {} epochs:".format(epoch))
-            print("\tTraining Cost: {}".format(l_train))
-            print("\tTraining Accuracy: {}".format(a_train))
-            print("\tValidation Cost: {}".format(l_valid))
-            print("\tValidation Accuracy: {}".format(a_valid))
-            if epoch < epochs:
+        saver_n = tf.train.import_meta_graph(load_path + '.meta')
+        saver_n.restore(sess, load_path)
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection('y')[0]
+        accuracy = tf.get_collection('accuracy')[0]
+        loss = tf.get_collection('loss')[0]
+        train_op = tf.get_collection('train_op')[0]
+        b_iter = X_train.shape[0] // batch_size
+        if b_iter % batch_size != 0:
+            b_iter += 1
+            fl = True
+        else:
+            fl = False
+        for i in range(epochs + 1):
+            cos_t, acc_t = sess.run([loss, accuracy], {x: X_train, y: Y_train})
+            cos_v, acc_v = sess.run([loss, accuracy], {x: X_valid, y: Y_valid})
+            print('After {} epochs:'.format(i))
+            print('\tTraining Cost: {}'.format(cos_t))
+            print('\tTraining Accuracy: {}'.format(acc_t))
+            print('\tValidation Cost: {}'.format(cos_v))
+            print('\tValidation Accuracy: {}'.format(acc_v))
+            if i < epochs:
                 x_tr, y_tr = shuffle_data(X_train, Y_train)
-                for iter in range(iterations):
-                    lower_bound = iter * batch_size
-                    if over_size is True and iter == iterations - 1:
-                        upper_bound = X_train.shape[0]
+                for j in range(b_iter):
+                    start = j * batch_size
+                    if j == b_iter - 1 and fl is True:
+                        final = X_train.shape[0]
                     else:
-                        upper_bound = iter * batch_size + batch_size
-                    batch_x = X_train[lower_bound:upper_bound]
-                    batch_y = Y_train[lower_bound:upper_bound]
+                        final = j * batch_size + batch_size
+                    batch_x = x_tr[start:final]
+                    batch_y = y_tr[start:final]
                     sess.run([train_op], {x: batch_x, y: batch_y})
-                    if epoch != 0 and (epoch + 1) % 100 == 0:
+                    if j != 0 and (j + 1) % 100 == 0:
                         batch_co, batch_ac = sess.run([loss, accuracy],
                                                       {x: batch_x, y: batch_y})
                         print('\tStep {}:'.format(j + 1))
