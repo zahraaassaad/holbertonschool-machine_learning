9329fc2ead1260645249fbf6020eb7a92f05ff78
zahraaassaad <zahraa.asaad8@gmail.com>
2021-01-21 14:37:56 +0000

    +=

--- supervised_learning/0x05-regularization/0-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -9,7 +9,8 @@ import numpy as np
 
 def l2_reg_cost(cost, lambtha, weights, L, m):
     """Returns: the cost of the network"""
+    l2 = 0
     for i in range(1, L+1):
         w = "W" + str(i)
-        l2 = 1/m * lambtha/2 * np.linalg.norm(weights[w])
+        l2 += 1/m * lambtha/2 * np.linalg.norm(weights[w])
     return cost + l2
