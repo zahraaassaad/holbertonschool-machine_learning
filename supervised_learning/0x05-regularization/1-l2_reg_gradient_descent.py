73bd53fb05d2f5d394f47f2f99625f15965a0e6c
zahraaassaad <zahraa.asaad8@gmail.com>
2021-01-21 15:55:56 +0000

    1

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -8,15 +8,13 @@ import numpy as np
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """gradient descent with L2 regularization"""
     weights_copy = weights.copy()
-    m = len(Y[0])
-    A = cache["A" + str(L)]
     dz = cache["A" + str(L)] - Y
     for i in range(L, 0, -1):
         A = cache["A" + str(i - 1)]
         w = "W" + str(i)
         b = "b" + str(i)
-        dw = (1 / len(Y[0])) * np.matmul(dz, A.T) + 1/m * lambtha * weights[w]
+        dw = (1 / len(Y[0])) * np.matmul(dz, A.T) + 1/len(Y[0]) * lambtha * weights[w]
         db = (1 / len(Y[0])) * np.sum(dz, axis=1, keepdims=True)
         weights[w] = weights[w] - alpha * dw
         weights[b] = weights[b] - alpha * db
-        dz = np.matmul(weights_copy["W" + str(i)].T, dz) * (A * (1 - A))
+        dz = np.matmul(weights_copy["W" + str(i)].T, dz) * (1 - A * A)
