7d304024bc2e5082cc9e35aee4d49e9e6781b507
zahraaassaad <zahraa.asaad8@gmail.com>
2021-01-21 16:45:55 +0000

    12

--- supervised_learning/0x05-regularization/2-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
-
-"""
-calculates the cost of a neural network
-"""
-
+"""calculates the cost """
 import tensorflow as tf
 
 
 def l2_reg_cost(cost):
-    """calculates the cost of a neural network"""
-    cost += tf.losses.get_regularization_losses()
-    return cost
+    """calculates the cost"""
+    regularization_losses = tf.losses.get_regularization_losses()
+    return cost + regularization_losses
