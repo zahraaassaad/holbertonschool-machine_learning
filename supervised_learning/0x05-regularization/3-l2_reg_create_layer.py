fb2478bf940a73aa0354fe8e750a08af09e66062
zahraaassaad <zahraa.asaad8@gmail.com>
2021-01-21 17:13:46 +0000

    3

---
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""creates a tensorflow layer
+"""
+
+import tensorflow as tf
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """Returns: the output of the new layer"""
+    w = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    l2_regu = tf.contrib.layers.l2_regularizer(lambtha)
+    layer = tf.layers.Dense(units=n, activation=activation,
+                            kernel_initializer=w, name="layer",
+                            kernel_regularizer=l2_regu)
+    y = layer(prev)
+    return (y)
