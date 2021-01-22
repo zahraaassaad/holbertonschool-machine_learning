a90fbfce6c7900df2535b6f6a658426c47047cc2
zahraaassaad <zahraa.asaad8@gmail.com>
2021-01-22 10:49:49 +0000

    0

---
+++ supervised_learning/0x03-optimization/0-cp.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""Calculates the normalization constants of a matrix."""
+import numpy as np
+
+
+def normalization_constants(X):
+    """Returns: the mean and standard deviation
+    """
+    mean = X.mean(axis=0)
+    stdev = X.std(axis=0)
+    return mean, stdev
--- supervised_learning/0x03-optimization/0-norm_constants.py
+++ supervised_learning/0x03-optimization/0-norm_constants.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
-"""Calculates the normalization constants of a matrix."""
+"""
+calculates the normalization constants
+"""
+
 import numpy as np
 
 
 def normalization_constants(X):
-    """Returns: the mean and standard deviation
-    """
-    mean = X.mean(axis=0)
-    stdev = X.std(axis=0)
+    """Returns: the mean and standard deviation"""
+    mean = np.mean(X, axis=0)
+    stdev = np.std(X, axis=0)
     return mean, stdev
