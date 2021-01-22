e47aa3d56be6c9799e9bfbf951f01822f6960085
zahraaassaad <zahraa.asaad8@gmail.com>
2021-01-22 11:30:35 +0000

    pep8

--- supervised_learning/0x02-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -9,4 +9,4 @@ def create_placeholders(nx, classes):
     """returns two placeholders"""
     x = tf.placeholder(tf.float32, shape=(None, nx), name="x")
     y = tf.placeholder(tf.float32, shape=(None, classes), name="y")
-    return x,y
+    return x, y
