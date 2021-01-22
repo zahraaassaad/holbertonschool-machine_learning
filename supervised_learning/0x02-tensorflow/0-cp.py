ff3a05b934fd198023b0109f7ef71e2f9b8fda4b
zahraaassaad <zahraa.asaad8@gmail.com>
2021-01-22 11:28:48 +0000

    0

--- supervised_learning/0x02-tensorflow/0-cp.py
+++ supervised_learning/0x02-tensorflow/0-cp.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-"""Returns: placeholders named x and y"""
-
+"""Returns two placeholders.
+"""
 import tensorflow as tf
 
 
 def create_placeholders(nx, classes):
-    """returns two placeholders"""
+    """Returns: placeholders named x and y."""
     x = tf.placeholder(tf.float32, shape=(None, nx), name="x")
     y = tf.placeholder(tf.float32, shape=(None, classes), name="y")
-    return x,y
+    return (x, y)
