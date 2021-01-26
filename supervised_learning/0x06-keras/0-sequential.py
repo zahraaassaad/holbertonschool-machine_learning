97c221ee0bf62c3d6e40770aa1dd3bf9cadff4a4
zahraaassaad <zahraa.asaad8@gmail.com>
2021-01-26 14:22:39 +0000

    k

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -8,7 +8,7 @@ import tensorflow.keras as K
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """Returns: the keras model"""
-    model = k.Sequential()
+    model = K.Sequential()
     regularization = K.regularizers.l2(lambtha)
     for i in range(len(layers)):
         if i == 0:
