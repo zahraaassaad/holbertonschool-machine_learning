28952f79425506a74f87f8a17d319a4b363e7b27
zahraaassaad <69789020+zahraaassaad@users.noreply.github.com>
2021-02-04 18:16:53 +0200

    Update 5-lenet5.py

--- supervised_learning/0x07-cnn/5-lenet5.py
+++ supervised_learning/0x07-cnn/5-lenet5.py
@@ -1,30 +1,36 @@
 #!/usr/bin/env python3
-""" builds a modified version of the LeNet-5
-    architecture using keras"""
+""" Module to create LeNet-5 with Keras"""
 import tensorflow.keras as K
 
 
 def lenet5(X):
-    """returns: K.Model compiled to use Adam optimization
-       (with default hyperparameters) and accuracy metrics"""
-    conv1 = K.layers.Conv2D(filters=6,
-                            kernel_size=(5, 5),
-                            activation='relu', padding="same",
-                            kernel_initializer="he_normal")(X)
-    pool1 = K.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2))(conv1)
-    conv2 = K.layers.Conv2D(filters=16, kernel_size=(5, 5),
-                            activation='relu', padding="valid",
-                            kernel_initializer="he_normal")(pool1)
-    pool2 = K.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2))(conv2)
-    pool2_flat = K.layers.Flatten()(pool2)
-    FL1 = K.layers.Dense(units=120, activation='relu',
-                         kernel_initializer="he_normal")(pool2_flat)
-    FL2 = K.layers.Dense(units=84, activation='relu',
-                         kernel_initializer="he_normal")(FL1)
-    FL3 = K.layers.Dense(units=10, activation='softmax',
-                         kernel_initializer="he_normal")(FL2)
-    opt = K.optimizers.Adam()
-    model = K.Model(inputs=X, outputs=FL3)
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=opt, metrics=["accuracy"])
+    """ Keras LeNet-5 """
+    init = K.initializers.he_normal(seed=None)
+    conv2d_1 = K.layers.Conv2D(filters=6, padding='same',
+                               kernel_initializer=init,
+                               kernel_size=5, activation='relu')(X)
+
+    pool_1 = K.layers.MaxPool2D(strides=2)(conv2d_1)
+
+    conv2d_2 = K.layers.Conv2D(filters=16, padding='valid',
+                               kernel_initializer=init,
+                               kernel_size=5, activation='relu')(pool_1)
+
+    pool_2 = K.layers.MaxPool2D(strides=2)(conv2d_2)
+
+    flat = K.layers.Flatten()(pool_2)
+
+    dense_1 = K.layers.Dense(units=120, kernel_initializer=init,
+                             activation='relu')(flat)
+
+    dense_2 = K.layers.Dense(units=84, kernel_initializer=init,
+                             activation='relu')(dense_1)
+
+    last_dense = K.layers.Dense(units=10, kernel_initializer=init,
+                                activation='softmax')(dense_2)
+
+    model = K.models.Model(inputs=X, outputs=last_dense)
+
+    model.compile(optimizer=K.optimizers.Adam(),
+                  loss='categorical_crossentropy', metrics=['accuracy'])
     return model
