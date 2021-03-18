17974e53f77580ed2170cbe7556061c6955a4c1b
zahraaassaad <69789020+zahraaassaad@users.noreply.github.com>
2021-03-18 17:06:27 +0200

    Update 0-determinant.py

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -12,10 +12,23 @@ def determinant(matrix):
     """
     Returns: the determinant of matrix
     """
-    if matrix == [[]]:
-        return 1
-    if len(matrix) == 0 or type(matrix) != list or type(matrix[0]) != list:
+    ind = list(range(len(matrix)))
+    total = 0
+    if matrix == []:
         raise TypeError("matrix must be a list of lists")
-    if len(matrix[0]) != len(matrix):
+    if len(matrix) != len(matrix[0]):
         raise ValueError("matrix must be a square matrix")
-    return int(LA.det(matrix))
+    if len(matrix[0]) == 0:
+        return 1
+    if len(matrix[0]) == 1:
+        return matrix[0][0]
+    if len(matrix[0]) == 2:
+        return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
+
+    for i in ind:
+        mat_copy = matrix
+        mat_copy = mat_copy[1:]
+        height = len(mat_copy)
+        for j in range(height):
+            mat_copy[j] = mat_copy[j][:i] + mat_copy[j][i+1:]
+    return total
