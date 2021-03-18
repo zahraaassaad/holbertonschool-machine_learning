617d84cf173c2afcb8b301565e892b23d4c2b2a6
zahraaassaad <69789020+zahraaassaad@users.noreply.github.com>
2021-03-18 15:26:19 +0200

    Create 0-determinant.py

---
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""
+0-determinant.py
+calculates the determinant of a matrix
+"""
+
+from numpy import linalg as LA
+
+
+def determinant(matrix):
+    """
+    Returns: the determinant of matrix
+    """
+    if matrix == [[]]:
+        return 1
+    if matrix == [] or type(matrix) != list or type(matrix[0]) != list:
+        raise TypeError("matrix must be a list of lists")
+    if len(matrix) != len(matrix[0]):
+        raise ValueError("matrix must be a square matrix")
+    return int(LA.det(matrix))
