c9a6ec6310b992562b42dc47a3407795d3e1764a
zahraaassaad <69789020+zahraaassaad@users.noreply.github.com>
2021-03-18 17:12:15 +0200

    Update 0-determinant.py

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -10,21 +10,23 @@ def determinant(matrix):
     """
     Returns: the determinant of matrix
     """
-    if not isinstance(matrix, list) or len(matrix) == 0:
+    ind = list(range(len(matrix)))
+    total = 0
+    if matrix == []:
         raise TypeError("matrix must be a list of lists")
-    if not all([isinstance(row, list) for row in matrix]):
-        raise TypeError("matrix must be a list of lists")
-    if len(matrix) == 1 and len(matrix[0]) == 0:
+    if len(matrix) != len(matrix[0]):
+        raise ValueError("matrix must be a square matrix")
+    if len(matrix[0]) == 0:
         return 1
-    if len(matrix) == 1 and len(matrix[0]) == 1:
+    if len(matrix[0]) == 1:
         return matrix[0][0]
-    square = [len(row) == len(matrix) for row in matrix]
-    if not all(square):
-        raise ValueError('matrix must be a square matrix')
-    if len(matrix) is 2:
-        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-    deter = 0
-    for c in range(len(matrix)):
-        deter += ((-1) ** c) * matrix[0][c] *\
-                 determinant(getMatrixMinor(matrix, 0, c))
-    return deter
+    if len(matrix[0]) == 2:
+        return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
+
+    for i in ind:
+        mat_copy = matrix
+        mat_copy = mat_copy[1:]
+        height = len(mat_copy)
+        for j in range(height):
+            mat_copy[j] = mat_copy[j][:i] + mat_copy[j][i+1:]
+    return total
