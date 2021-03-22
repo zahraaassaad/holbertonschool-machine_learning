6b7d6d581ab211bcc8e19e05abe80c83424c1461
zahraaassaad <69789020+zahraaassaad@users.noreply.github.com>
2021-03-22 18:18:36 +0200

    Update 0-mean_cov.py

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -15,11 +15,7 @@ def mean_cov(X):
         raise ValueError("X must contain multiple data points")
     mean = np.array([sum(X)/len(X)])
     rows, cols = X.shape
-    cov_mat = np.zeros((cols, cols))
-    for i in range(cols):
-        for j in range(cols):
-            # store the value in the matrix
-            mean_x, mean_y = X[:, i].mean(), X[:, j].mean()
-            n = len(X[:, i])
-            cov_mat[i][j] = sum((X[:, i] - mean_x) * (X[:, j] - mean_y)) / n
-    return mean, cov_mat
+    a = X - mean
+    c = np.matmul(a.T, a)
+    cov = c/(rows - 1)
+    return mean, cov
