580f321a4a51bad505ddf801d8d11042cf238203
zahraaassaad <69789020+zahraaassaad@users.noreply.github.com>
2021-03-22 17:54:51 +0200

    Update 0-mean_cov.py

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -13,7 +13,7 @@ def mean_cov(X):
         raise TypeError("X must be a 2D numpy.ndarray")
     if len(X) < 2:
         raise ValueError("X must contain multiple data points")
-    mean = sum(X)/len(X)
+    mean = np.array([sum(X)/len(X)])
     rows, cols = X.shape
     cov_mat = np.zeros((cols, cols))
     for i in range(cols):
