ad18ef5feced9d1aa4a158869fb789912add2da8
zahraaassaad <69789020+zahraaassaad@users.noreply.github.com>
2021-03-22 18:21:17 +0200

    Update 0-mean_cov.py

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -10,7 +10,7 @@ def mean_cov(X):
     """Returns: mean, cov"""
     if type(X) != numpy.ndarray:
         raise TypeError("X must be a 2D numpy.ndarray")
-    if len(X) < 2:
+    if X.shape[0] < 2:
         raise ValueError("X must contain multiple data points")
     mean = np.mean(X, axis=0)
     rows, cols = X.shape
