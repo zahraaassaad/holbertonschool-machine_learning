f595e706dd66cf2e09fc81e5d0b055bafb6662e2
zahraaassaad <69789020+zahraaassaad@users.noreply.github.com>
2021-03-22 19:00:55 +0200

    Update 1-correlation.py

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -7,8 +7,10 @@ def correlation(C):
     """
     C is a numpy.ndarray of shape (d, d) containing a covariance matrix
     d is the number of dimensions
-    If C is not a numpy.ndarray, raise a TypeError with the message C must be a numpy.ndarray
-    If C does not have shape (d, d), raise a ValueError with the message C must be a 2D square matrix
+    If C is not a numpy.ndarray:
+        raise a TypeError with the message C must be a numpy.ndarray
+    If C does not have shape (d, d):
+        raise a ValueError with the message C must be a 2D square matrix
     Returns:
         a numpy.ndarray of shape (d, d) containing the correlation matrix
     """
