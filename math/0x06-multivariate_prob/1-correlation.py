f1342e080df0629a254c19868a2eaf83d13f9fd3
zahraaassaad <69789020+zahraaassaad@users.noreply.github.com>
2021-03-22 19:01:45 +0200

    Update 1-correlation.py

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -16,7 +16,7 @@ def correlation(C):
     """
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
-    if C.shape[0] != C.shape[1]:
+    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
     diag = np.diag(C)
     sqrt = np.sqrt(diag)
