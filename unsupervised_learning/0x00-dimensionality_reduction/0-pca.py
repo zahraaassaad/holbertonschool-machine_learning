b9e83e9a9b668303de8806afc4ccba4182e819ab
zahraaassaad <69789020+zahraaassaad@users.noreply.github.com>
2021-03-26 21:23:51 +0200

    Create 0-pca.py

---
+++ unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" calculates the mean and covariance of a data set """
+import numpy as np
+
+
+def mean_cov(X):
+    """
+  X is a numpy.ndarray of shape (n, d) where:
+  n is the number of data points
+  d is the number of dimensions in each point
+  all dimensions have a mean of 0 across all data points
+  var is the fraction of the variance
+    that the PCA transformation should maintain
+  Returns:
+    the weights matrix, W, that maintains var fraction of X‘s original variance
+  W is a numpy.ndarray of shape (d, nd)
+    where nd is the new dimensionality of the transformed X
+   """
+    U, S, V = np.linalg.svd(X)
+    sum = np.cumsum(S)
+    num = []
+    for i in range(len(S)):
+        if ((sum[i]) / sum[-1]) >= var:
+            num.append(i)
+    dims = num[0] + 1
+    return V.T[:, :dims]
