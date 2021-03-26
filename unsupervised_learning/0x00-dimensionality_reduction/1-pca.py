764489418792c355798a97ae80985bb0224e85e5
zahraaassaad <69789020+zahraaassaad@users.noreply.github.com>
2021-03-26 21:37:13 +0200

    Create 1-pca.py

---
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""performs PCA on a dataset"""
+
+import numpy as np
+
+
+def pca(X, ndim):
+    """
+  X is a numpy.ndarray of shape (n, d) where:
+  n is the number of data points
+  d is the number of dimensions in each point
+  ndim is the new dimensionality of the transformed X
+  Returns:
+  T, a numpy.ndarray of shape (n, ndim)
+  containing the transformed version of X
+   """
+    data = X - np.mean(X, axis=0)
+    u, S, v = np.linalg.svd(data)
+    W = v[:ndim].T
+    return np.matmul(data, W)
