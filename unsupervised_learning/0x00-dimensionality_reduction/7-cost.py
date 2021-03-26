fe6802951adff56abe27a14b9bf8f1732ce03160
zahraaassaad <69789020+zahraaassaad@users.noreply.github.com>
2021-03-26 21:53:17 +0200

    Create 7-cost.py

---
+++ unsupervised_learning/0x00-dimensionality_reduction/7-cost.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Function that calculates the cost of the t-SNE transformation"""
+
+import numpy as np
+
+
+def cost(P, Q):
+    """
+    P is a numpy.ndarray of shape (n, n) containing the P affinities
+    Q is a numpy.ndarray of shape (n, n) containing the Q affinities
+    Returns: C, the cost of the transformation
+    """
+    P = np.maximum(P, 1e-12)
+    Q = np.maximum(Q, 1e-12)
+    C = np.sum(P * np.log(P / Q))
+    return C
