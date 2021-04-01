f049a9b5bdff9a8074e36d44a7195787845230c4
zahraaassaad <69789020+zahraaassaad@users.noreply.github.com>
2021-04-01 20:50:19 +0300

    Update 8-EM.py

--- unsupervised_learning/0x01-clustering/8-EM.py
+++ unsupervised_learning/0x01-clustering/8-EM.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" performs the expectation maximization for a GMM """
+"""Function that performs the expectation maximization for a GMM"""
+
 import numpy as np
 initialize = __import__('4-initialize').initialize
 expectation = __import__('6-expectation').expectation
@@ -7,50 +8,58 @@ maximization = __import__('7-maximization').maximization
 
 
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
-    """
-    X is a numpy.ndarray of shape (n, d) containing the data set
+    """X is a numpy.ndarray of shape (n, d) containing the data set
     k is a positive integer containing the number of clusters
-    iterations is a positive integer containing the maximum number of
-        iterations for the algorithm
-    tol is a non-negative float containing tolerance of the log likelihood,
-        used to determine early stopping i.e. if the difference is less than
-        or equal to tol you should stop the algorithm
-    verbose is a boolean that determines if you should print information about
-        the algorithm
-        If True, print Log Likelihood after {i} iterations: {l} every 10
-            iterations and after the last iteration
+    iterations is a positive integer containing the
+    maximum number of iterations for the algorithm
+    tol is a non-negative float containing tolerance
+    of the log likelihood, used to determine early
+    stopping i.e. if the difference is less than or
+    equal to tol you should stop the algorithm
+    verbose is a boolean that determines if you
+    should print information about the algorithm
+        If True, print Log Likelihood after {i} iterations:
+        {l} every 10 iterations and after the last iteration
         {i} is the number of iterations of the EM algorithm
         {l} is the log likelihood, rounded to 5 decimal places
+    You should use:
+        initialize = __import__('4-initialize').initialize
+        expectation = __import__('6-expectation').expectation
+        maximization = __import__('7-maximization').maximization
+    You may use at most 1 loop
     Returns: pi, m, S, g, l, or None, None, None, None, None on failure
-        pi is a numpy.ndarray of shape (k,) containing the priors for each
-            cluster
-        m is a numpy.ndarray of shape (k, d) containing the centroid means for
-            each cluster
-        S is a numpy.ndarray of shape (k, d, d) containing the covariance
-            matrices for each cluster
-        g is a numpy.ndarray of shape (k, n) containing the probabilities for
-            each data point in each cluster
-        l is the log likelihood of the model
-    """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-        return None, None, None, None, None
-    if not isinstance(k, int) or k <= 0 or X.shape[0] < k:
-        return None, None, None, None, None
-    if not isinstance(iterations, int) or iterations <= 0:
-        return None, None, None, None, None
-    if not isinstance(tol, float) or tol < 0:
-        return None, None, None, None, None
-    if not isinstance(verbose, bool):
-        return None, None, None, None, None
+        pi is a numpy.ndarray of shape (k,)
+        containing the priors for each cluster
+        m is a numpy.ndarray of shape (k, d)
+        containing the centroid means for each cluster
+        S is a numpy.ndarray of shape (k, d, d)
+        containing the covariance matrices for each cluster
+        g is a numpy.ndarray of shape (k, n) containing
+        the probabilities for each data point in each cluster
+        l is the log likelihood of the model"""
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return (None, None, None, None, None)
+    if type(k) is not int or type(iterations) is not int:
+        return (None, None, None, None, None)
+    if k <= 0 or iterations <= 0:
+        return (None, None, None, None, None)
+    if type(tol) is not float or tol < 0:
+        return (None, None, None, None, None)
+    if type(verbose) is not bool:
+        return (None, None, None, None, None)
+    n, d = X.shape
     pi, m, S = initialize(X, k)
-    l_prev = 0
+    g, ll = expectation(X, pi, m, S)
+    ll_old = 0
+    text = 'Log Likelihood after {} iterations: {}'
     for i in range(iterations):
-        g, l_model = expectation(X, pi, m, S)
+        if verbose and i % 10 == 0:
+            print(text.format(i, ll.round(5)))
         pi, m, S = maximization(X, g)
-        if abs(l_prev - l_model) < tol and verbose:
-            print("Log Likelihood after {} iterations: {}".format(i, l_model))
+        g, ll = expectation(X, pi, m, S)
+        if np.abs(ll_old - ll) <= tol:
             break
-        if (i % 10 == 0 or i == iterations - 1) and verbose:
-            print("Log Likelihood after {} iterations: {}".format(i, l_model))
-        l_prev = l_model
-    return pi, m, S, g, l_model
+        ll_old = ll
+    if verbose:
+        print(text.format(i + 1, ll.round(5)))
+    return pi, m, S, g, ll
