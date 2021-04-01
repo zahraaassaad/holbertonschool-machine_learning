6f50724a988427bba705d27afc9235329b621f1d
zahraaassaad <69789020+zahraaassaad@users.noreply.github.com>
2021-04-01 20:40:46 +0300

    Update 4-initialize.py

--- unsupervised_learning/0x01-clustering/4-initialize.py
+++ unsupervised_learning/0x01-clustering/4-initialize.py
@@ -1,30 +1,27 @@
 #!/usr/bin/env python3
-"""Function that initializes variables for a Gaussian Mixture Model"""
-
+""" initializes variables for a Gaussian Mixture Model"""
 import numpy as np
 kmeans = __import__('1-kmeans').kmeans
 
 
 def initialize(X, k):
-    """X is a numpy.ndarray of shape (n, d) containing the data set
+    """
+    X is a numpy.ndarray of shape (n, d) containing the data set
     k is a positive integer containing the number of clusters
-    You are not allowed to use any loops
     Returns: pi, m, S, or None, None, None on failure
-        pi is a numpy.ndarray of shape (k,) containing
-        the priors for each cluster, initialized evenly
-        m is a numpy.ndarray of shape (k, d) containing the
-        centroid means for each cluster, initialized with K-means
+        pi is a numpy.ndarray of shape (k,) containing the priors for each
+            cluster, initialized evenly
+        m is a numpy.ndarray of shape (k, d) containing the centroid means
+            for each cluster, initialized with K-means
         S is a numpy.ndarray of shape (k, d, d) containing the covariance
-        matrices for each cluster, initialized as identity matrices
-    You should use kmeans = __import__('1-kmeans').kmeans"""
-    if type(X) is not np.ndarray or type(k) is not int:
-        return (None, None, None)
-    if len(X.shape) != 2 or k <= 0:
-        return (None, None, None)
+            matrices for each cluster, initialized as identity matrices
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None, None
+    if not isinstance(k, int) or k <= 0 or X.shape[0] < k:
+        return None, None, None
     n, d = X.shape
+    pi = np.full((k,), 1/k)
     m, _ = kmeans(X, k)
-    S = np.zeros((k, d, d))
-    S[:] = np.identity(d)
-    pi = np.zeros((k))
-    pi[:] = 1 / k
-    return (pi, m, S)
+    S = np.tile(np.identity(d), (k, 1)).reshape(k, d, d)
+    return pi, m, S
