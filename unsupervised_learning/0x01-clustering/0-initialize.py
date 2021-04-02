670c38ef3e54b369d42496f9a09b1ca2ffba90fb
zahraaassaad <69789020+zahraaassaad@users.noreply.github.com>
2021-04-02 16:55:45 +0300

    Update 0-initialize.py

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -1,32 +1,25 @@
 #!/usr/bin/env python3
-"""Function that initializes cluster centroids for K-means"""
+
+"""
+initializes cluster centroids for K-means
+"""
+
 
 import numpy as np
 
 
 def initialize(X, k):
-    """X is a numpy.ndarray of shape (n, d) containing
-    the dataset that will be used for K-means clustering
-        n is the number of data points
-        d is the number of dimensions for each data point
-    k is a positive integer containing the number of clusters
-    The cluster centroids should be initialized with a
-    multivariate uniform distribution along each dimension in d:
-        The minimum values for the distribution should be
-        the minimum values of X along each dimension in d
-        The maximum values for the distribution should be
-        the maximum values of X along each dimension in d
-        You should use numpy.random.uniform exactly once
-    You are not allowed to use any loops
-    Returns: a numpy.ndarray of shape (k, d) containing the
-    initialized centroids for each cluster, or None on failure"""
-    if type(X) is not np.ndarray or type(k) is not int:
+    """
+    Returns: a numpy.ndarray of shape (k, d)
+        containing the initialized centroids for each cluster,
+        or None on failure
+    """
+    if type(X) != np.ndarray or len(X.shape) != 2:
         return None
-    if len(X.shape) != 2 or k < 0:
+    if type(k) != int or k <= 0:
         return None
-    n, d = X.shape
-    if k == 0:
-        return None
-    low = np.amin(X, axis=0)
-    high = np.amax(X, axis=0)
-    return np.random.uniform(low, high, size=(k, d))
+    n, d = np.shape(X)
+    l = np.min(X, axis=0)
+    h = np.max(X, axis=0)
+    centroids = np.random.uniform(l, h, size=(k, d))
+    return centroids
