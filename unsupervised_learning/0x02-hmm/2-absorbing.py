6efce17d3c4d5e26aa8d07d0e2e32b19190cf84b
zahraaassaad <69789020+zahraaassaad@users.noreply.github.com>
2021-04-08 21:16:22 +0300

    Create 2-absorbing.py

---
+++ unsupervised_learning/0x02-hmm/2-absorbing.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Function that determines if a markov chain is absorbing"""
+
+import numpy as np
+
+
+def absorbing(P):
+    """Returns: True if it is absorbing, or False on failure"""
+    if type(P) is not np.ndarray or len(P.shape) != 2:
+        return False
+    if P.shape[0] != P.shape[1]:
+        return False
+    if np.min(P ** 2) < 0 or np.min(P ** 3) < 0:
+        return False
+    ab_state = np.where(np.diag(P) == 1)
+    if len(ab_state[0]) == P.shape[0]:
+        return True
+    if len(ab_state[0]) == 0:
+        return False
+    B = np.copy(P)
+    B = np.delete(np.delete(B, ab_state[0], 0), ab_state[0], 1)
+    In = np.identity(B.shape[0])
+    try:
+        result = np.linalg.inv(In - B)
+        return True
+    except Exception:
+        return False
